0aec2fd098f7d0c3b7969b716d7630f4d3e3ea40
Torf <mail@torf.cc>
2017-09-24 19:25:11 +0200

    Fix timing issue in initialization

--- src/tooltip/tooltip.js
+++ src/tooltip/tooltip.js
@@ -3,31 +3,7 @@
  * Diese Datei händelt die Funktionen für das ToolTip
  */
 
-var toolTip = document.getElementById('tool-tip'),
-    currentConstituencyNumber = null,
-    currentConstituencyObject = null,
-    xpos = 0,
-    ypos = 0;
-
-/**
- * Listener der schaut wo der User gerade mit der Maus ist
- */
-document.body.addEventListener('mousemove',function(mouseEvent){
-
-    // Prüfen ob der Brwoser das Objekt untersützt
-    if (mouseEvent) {
-        xpos = mouseEvent.clientX;
-        ypos = mouseEvent.clientY;
-    } else {
-        //IE
-        xpos = window.event.clientX;
-        ypos = window.event.clientY;
-    } // end if else
-
-    // Position von der Maus dem Div übergeben
-    toolTip.style.top   = (ypos - (toolTip.offsetHeight / 2))+'px';
-    toolTip.style.left  = (xpos + 25)+'px';
-},false);
+var toolTip;
 
 
 /**
